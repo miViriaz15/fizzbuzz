999e4e9af1be652a0f9527181db84c7216360714
miViriaz15 <46974607+miViriaz15@users.noreply.github.com>
2019-02-16 07:40:24 -0500

    Updated from Brython Server: 2/16/2019 7:40:24 AM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -53,10 +53,10 @@ numbers=range(1, numbers+1)
 for i in numbers:
     if i//3==0 and i//5==0:
         print('FizzBuzz')
-            elif i//5==0:
-                print('Buzz")
-                    elif i//3==0:
-                        print('Fizz')
-                            else:
-                                print(i)
+    elif i//5==0:
+        print('Buzz')
+    elif i//3==0:
+        print('Fizz')
+    else:
+        print(i)
                             
